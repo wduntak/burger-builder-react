cd9232399ac71445ae312845f6c0ba20ec50d0b9
Wangdu Duntak <wduntak@kobo.com>
2017-12-10 13:39:15 -0500

    Add basic components and styles, setup ingredients with state

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map( ingredientKey => {
+            return [...Array( props.ingredients[ingredientKey] )].map( (e, i) => {
+                return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+            } );
+        } );
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+};
+
+export default burger;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+import classes from './Layout.css';
+
+const layout = ( props ) => (
+    <Fragment>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={classes.content}>
+            {props.children}
+        </main>
+    </Fragment>
+);
+
+export default layout;
