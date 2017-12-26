b31aa486029c3cab92b76b91e56e7f7048e1f1b4
Wangdu Duntak <wduntak@kobo.com>
2017-12-26 12:14:37 -0500

    Change height in logo component to be styled by components that use logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ import classes from './Logo.css';
 
 const logo = (props) => (
     <div className={classes.Logo}>
-        <img src={burgerLogo} alt="Burger Logo"/>
+        <img src={burgerLogo} alt="Burger Logo" />
     </div>
 )
 
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,7 +6,9 @@ import classes from './Toolbar.css';
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>Menu</div>
-        <Logo />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
         <nav>
             <NavigationItems />
         </nav>
